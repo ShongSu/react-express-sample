a973648dcc09400035c06b531f2db9a76c0ff18a
Shongsu <chinhouu1992@gmail.com>
2020-09-28 15:36:47 -0400

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
